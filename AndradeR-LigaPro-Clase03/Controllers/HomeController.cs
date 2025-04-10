2b56e58eba64142e77cf5a897193f6a7020dc997
Rodrigo Andrade <rodrigo.andrade@hotmail.com>
2025-04-10 07:37:19 -0500

    Agregar archivos de proyecto.

---
+++ AndradeR-LigaPro-Clase03/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using AndradeR_LigaPro_Clase03.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AndradeR_LigaPro_Clase03.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AndradeR-LigaPro-Clase03/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AndradeR_LigaPro_Clase03.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
